92b4ec6fdebc70b122e97ed3d06f2184a4f46493
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-07-14 14:25:22 +0000

    passed callback and show props

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
+import Modal from "./Modal";
 
 const App = () => {
+
+  const[show,setShow]=useState(false);
+
+  function onClose(){
+    setShow(false);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        {/* <h1>hello</h1> */}
+        <button onClick={()=>setShow(true)} style={{backgroundColor:'tomato',color:'white'}}>Show Modal</button>
+        <Modal show={show} onClose={onClose}/>
     </div>
   )
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Modal=(props)=>{
+
+console.log(props);
+const{show,onClose}=props;
+
+    return (
+        <>
+ {show && <div  style={{backgroundColor:'royalblue',padding:'1rem'}}>
+    <button onClick={()=>onClose()} style={{backgroundColor:'tomato',color:'white'}}>Close</button>
+    <p >This is the content of the modal</p>
+ </div>}
+        </>
+
+    );
+}
+export default Modal;
